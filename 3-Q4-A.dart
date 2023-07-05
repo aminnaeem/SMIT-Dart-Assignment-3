1c342a402877bf292f01fd452393700d5667ecc8
aminnaeem <amin.naeem1233@gmail.com>
2023-07-05 22:59:36 +0500

    SMIT Dart Assignment#3 Class 6

---
+++ 3-Q4-A.dart
@@ -0,0 +1,21 @@
+// For Factorial
+
+int Factorial(int n) {
+  int result = 1;
+  int i = 1;
+
+  while(i <= n){
+    result *= i;
+    i++;
+  }
+  return result;
+}
+
+void main () {
+
+  int n = 5;
+  int result = Factorial(n);
+
+  print('The Factorial of $n is $result');
+
+}
