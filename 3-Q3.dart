bd9914ef10fff020c14cba17a82d87e91d695fea
aminnaeem <amin.naeem1233@gmail.com>
2023-07-05 22:07:13 +0500

    SMIT Dart Assignment#3 Class 6

---
+++ 3-Q3.dart
@@ -0,0 +1,24 @@
+bool isPrime(n) {
+  for (var i = 2; i <= n/i; ++i) {
+    if(n%i == 0){
+      return false;
+    }
+  }
+  return true;
+}
+
+void main() {
+
+  int n = 17;
+  String Output = '';
+
+  if(isPrime(n)){
+    Output = '$n is a Prime Number';
+  }
+
+  else{
+    Output = '$n is not a Prime Number';
+  }
+  print(Output);
+
+}
