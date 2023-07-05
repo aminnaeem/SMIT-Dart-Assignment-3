e83f23c2aeeb0b631b896762679afa505d327c37
aminnaeem <amin.naeem1233@gmail.com>
2023-07-05 20:32:39 +0500

    SMIT Dart Assignment#3 Class 6

---
+++ 3-Q1.dart
@@ -0,0 +1,15 @@
+void main() {
+
+  List Numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  List EvenNums = [];
+
+  for (var i = 0; i < Numbers.length; i++) {
+    
+    if(Numbers[i]%2==0){
+      EvenNums.add(Numbers[i]);
+    }
+  }
+
+  print(EvenNums);
+
+}
