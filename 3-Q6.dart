b9669a9e221e8df5c74b736380431cbe3a254bdd
aminnaeem <amin.naeem1233@gmail.com>
2023-07-06 20:12:02 +0500

    SMIT Dart Assignment#3 Class 6

---
+++ 3-Q6.dart
@@ -0,0 +1,16 @@
+void main() {
+
+  List val = [3, 9, 1, 6, 4, 2, 8, 5, 7];
+  int LargestNumber = val[0];
+
+
+  for (var i = 1; i < val.length; i++) {
+
+    if(val[i] > LargestNumber) {
+      LargestNumber = val[i];
+    }
+    
+  }
+  print(LargestNumber);
+
+}
