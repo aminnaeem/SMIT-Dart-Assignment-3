fa4a8847077f53967ace9f6bf6e355bfb0985951
aminnaeem <amin.naeem1233@gmail.com>
2023-07-09 22:23:05 +0500

    SMIT Dart Assignment#3 Class 6

---
+++ 3-Q24.dart
@@ -0,0 +1,26 @@
+void main () {
+
+  List<int> numbers = [-23, 30, 42, -29, -67, -44, 51];
+  int sum = 0;
+  int count = 0;
+
+  for (var i = 0; i < numbers.length; i++) {
+    int number = numbers[i];
+    if (number < 0) {
+
+      sum += number;
+      count++;
+      
+    }
+  }
+  // double average = count > 0 ? sum / count : 0 ;
+  double average = 0;
+  if (count > 0) {
+    average = sum / count;
+  }
+  else{
+    average = 0;
+  }
+  print('Average of negative numbers: $average');
+
+}
